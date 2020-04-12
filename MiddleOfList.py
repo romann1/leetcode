48a6511b5bfa8395941042b00d735191f286bf33
roma <roma>
2020-04-12 19:20:18 -0400

    initial commit

---
+++ MiddleOfList.py
@@ -0,0 +1,52 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    @staticmethod
+    def make_list(in_list):
+        head = ListNode(None)
+        tail = ListNode(None)
+
+        for x in in_list:
+            node = ListNode(x)
+            if head.val is None:
+                head = node
+                tail = head
+            else:
+                tail.next = node
+                tail = node
+        return head
+
+    def __repr__(self):
+        s = '['
+        s = s + str(self.val) + ','
+        node = self.next
+        while node is not None:
+            s = s + str(node.val) + ','
+            node = node.next
+        s = s[0:-1] + ']'
+
+        return s
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        ptr = head
+        mid = head
+
+        i = 0
+        while ptr is not None:
+            i += 1
+            if i % 2 == 0:
+                mid = mid.next
+            ptr = ptr.next
+
+        return mid
+
+sol = Solution()
+
+res_list = sol.middleNode(ListNode.make_list([1, 2, 3, 4, 5]))
+print(f'{res_list}')
+res_list = sol.middleNode(ListNode.make_list([1, 2, 3, 4, 5, 6]))
+print(f'{res_list}')
